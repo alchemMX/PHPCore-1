92f1b0d347c2dbc8610398d63214f67fdac0dbc4
Infin48 <patrik.kad@gmail.com>
2021-05-08 13:40:59 +0200

    Update

--- Install/Update.php
+++ Install/Update.php
@@ -1,7 +1,7 @@
 <?php
 
-$this->db->query('ALTER TABLE ' . TABLE_PROFILE_POSTS . ' DROP IF EXISTS profile_post_edited');
-$this->db->query('ALTER TABLE ' . TABLE_PROFILE_POSTS . ' DROP IF EXISTS is_edited');
+$this->db->query('ALTER TABLE ' . TABLE_PROFILE_POSTS . ' DROP COLUMN IF EXISTS profile_post_edited');
+$this->db->query('ALTER TABLE ' . TABLE_PROFILE_POSTS . ' DROP COLUMN IF EXISTS is_edited');
 
-$this->db->query('ALTER TABLE ' . TABLE_PROFILE_POSTS_COMMENTS . ' DROP IF EXISTS profile_post_comment_edited');
-$this->db->query('ALTER TABLE ' . TABLE_PROFILE_POSTS_COMMENTS . ' DROP IF EXISTS is_edited');
+$this->db->query('ALTER TABLE ' . TABLE_PROFILE_POSTS_COMMENTS . ' DROP COLUMN IF EXISTS profile_post_comment_edited');
+$this->db->query('ALTER TABLE ' . TABLE_PROFILE_POSTS_COMMENTS . ' DROP COLUMN IF EXISTS is_edited');
