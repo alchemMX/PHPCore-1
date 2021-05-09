478a3c5c2131b1f0a6ead11c3b4378bfdf6f0e8a
Infin48 <patrik.kad@gmail.com>
2021-05-09 13:42:18 +0200

    Update

--- Install/Update.php
+++ Install/Update.php
@@ -11,4 +11,4 @@ $this->db->query('ALTER TABLE ' . explode(' ', TABLE_FORUMS)[0] . ' CHANGE COLUM
 $this->db->query('ALTER TABLE ' . explode(' ', TABLE_REPORTS_REASONS)[0] . ' CHANGE COLUMN IF EXISTS `reason_id` report_reason_id bigint(11) NOT NULL AUTO_INCREMENT');
 $this->db->query('ALTER TABLE ' . explode(' ', TABLE_REPORTS_REASONS)[0] . ' CHANGE COLUMN IF EXISTS `reason_text` report_reason_text varchar(225) NOT NULL COLLATE utf8_general_ci');
 $this->db->query('ALTER TABLE ' . explode(' ', TABLE_REPORTS_REASONS)[0] . ' CHANGE COLUMN IF EXISTS `reason_type` report_reason_type tinyint(1) NOT NULL');
-$this->db->query('ALTER TABLE ' . explode(' ', TABLE_REPORTS_REASONS)[0] . ' CHANGE COLUMN IF EXISTS `reason_time` report_reason_time  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP');
+$this->db->query('ALTER TABLE ' . explode(' ', TABLE_REPORTS_REASONS)[0] . ' CHANGE COLUMN IF EXISTS `report_reason_time` report_reason_created  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP');
