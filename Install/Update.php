5770e19d397a69224c19070d7625e39eefcf512d
Infin48 <patrik.kad@gmail.com>
2021-05-09 17:56:03 +0200

    Update

--- Install/Update.php
+++ Install/Update.php
@@ -1,1 +1,3 @@
 <?php
+
+$this->db->query('ALTER TABLE ' . explode(' ', TABLE_REPORTS_REASONS)[0] . ' CHANGE COLUMN IF EXISTS `report_reason_type` report_reason_type tinyint(1) NOT NULL DEFAULT "0"');
